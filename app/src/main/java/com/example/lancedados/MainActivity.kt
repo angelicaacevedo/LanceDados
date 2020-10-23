64172c69b2a8dc7478cab742112c284cd3178682
Angélica Acevedo <aceroacevedoangelica@gmail.com>
2020-10-22 22:17:38 -0300

    Adição de Dados e correção de código

--- app/src/main/java/com/example/lancedados/MainActivity.kt
+++ app/src/main/java/com/example/lancedados/MainActivity.kt
@@ -11,12 +11,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
-        val valorDado1 =findViewById<TextView>(R.id.dado_1)
-        val valorDado2 =findViewById<TextView>(R.id.dado_2)
 
                 botaoLancarDados.setOnClickListener {
-                    valorDado1.text = lancarDados().toString()
-                    valorDado2.text = lancarDados().toString()
+
 
         }
     }
