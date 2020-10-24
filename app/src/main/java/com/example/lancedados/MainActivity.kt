5b654b98c92e4a7cdcaf7982dca93bd48400b098
Angélica Acevedo <aceroacevedoangelica@gmail.com>
2020-10-24 09:27:04 -0300

    Dados com a funçao aleatória

--- app/src/main/java/com/example/lancedados/MainActivity.kt
+++ app/src/main/java/com/example/lancedados/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lancedados
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +12,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
+        val dado1 = findViewById<ImageView>(R.id.dado1)
+        val dado2 = findViewById<ImageView>(R.id.dado2)
 
-                botaoLancarDados.setOnClickListener {
-
+        botaoLancarDados.setOnClickListener {
+            dado1.setImageResource(escolherImagem())
+            dado2.setImageResource(escolherImagem())
 
         }
     }
-        fun lancarDados():Int {
-            return (1..6).random()
+
+    fun escolherImagem(): Int {
+        return when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+    }
 }
